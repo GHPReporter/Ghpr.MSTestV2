99a5b2aaf40b20126428d96c508945232c97ec89
Evgeniy Kosjakov <42_@mail.ru>
2018-09-17 15:20:27 +0300

    console for debug added

---
+++ Ghpr.MSTestV2/ConsoleAppForDebug/Program.cs
@@ -0,0 +1,13 @@
+﻿using Ghpr.MSTestV2.Utils;
+
+namespace ConsoleAppForDebug
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var path = args[0];
+            GhprMSTestV2RunHelper.CreateReportFromFile(path, new EmptyTestDataProvider());
+        }
+    }
+}
